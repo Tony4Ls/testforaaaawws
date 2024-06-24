fa3640661bb5ca4cc8451152d7621fd0801552ec
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-24 22:26:10 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -136,9 +136,9 @@ resource "aws_eks_node_group" "eks_node_group" {
   subnet_ids      = aws_subnet.private[*].id
 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
+    desired_size = 2
+    max_size     = 3
+    min_size     = 0
   }
 
   instance_types = ["t3.medium"]
