b5fccdb2b758eeb3b5c8ba111be952bf22f1ed94
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 15:09:13 +0800

    Delete terraform directory

--- terraform/cloudfront_route53/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "alb_dns_name" {
-  description = "The DNS name of the ALB"
-  type        = string
-}
-
-variable "domain_name" {
-  description = "The DNS name for the Route 53 hosted zone"
-  type        = string
-}
