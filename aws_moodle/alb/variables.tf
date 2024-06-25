6d715379edd32688c4520d6515f854a339ee63a3
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 21:06:36 +0800

    Create variables.tf

---
+++ aws_moodle/alb/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
