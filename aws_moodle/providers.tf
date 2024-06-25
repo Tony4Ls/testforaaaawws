ebceee0ea86395b1d1426f74afb1ae820e50e4a7
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 23:37:37 +0800

    Update providers.tf

--- aws_moodle/providers.tf
+++ aws_moodle/providers.tf
@@ -1,17 +1,15 @@
 provider "aws" {
-  region = var.aws_region
+  region = var.region
 }
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.eks_cluster.name
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    host                   = aws_eks_cluster.eks_cluster.endpoint
     token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
   }
 }
