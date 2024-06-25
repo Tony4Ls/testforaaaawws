cade85012250b5231f8ac9d330b3d031839632ba
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 20:53:00 +0800

    Update main.tf

--- aws_moodle/eks/main.tf
+++ aws_moodle/eks/main.tf
@@ -2,7 +2,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "18.0.0"
   cluster_name    = var.cluster_name
-  cluster_version = "1.21"
+  cluster_version = "1.30"
   subnets         = var.private_subnets
   vpc_id          = var.vpc_id
 
