dac11604c128a2d4af06f9fc6e5128329d949602
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 23:45:55 +0800

    Update main.tf

--- aws_moodle/eks/main.tf
+++ aws_moodle/eks/main.tf
@@ -1,33 +1,42 @@
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "18.0.0"
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
-  vpc_id          = var.vpc_id
-  subnet_ids      = var.private_subnets
-
-  eks_managed_node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_type    = "t3.medium"
-    }
+resource "aws_security_group" "eks_cluster_sg" {
+  vpc_id = var.vpc_id
+  tags = {
+    Name = "moodle-eks-cluster-sg"
   }
 }
 
-output "cluster_name" {
-  value = module.eks.cluster_id
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.cluster_name
+  role_arn = data.aws_iam_role.lab_role.arn
+
+  vpc_config {
+    subnet_ids         = var.private_subnets
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
+  }
 }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "moodle-node-group"
+  node_role_arn   = data.aws_iam_role.lab_role.arn
+  subnet_ids      = var.private_subnets
 
-output "cluster_certificate_authority" {
-  value = module.eks.cluster_certificate_authority_data
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 0
+  }
+
+  instance_types = ["t3.medium"]
+
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy       = false
+  }
+
+  depends_on = [aws_eks_cluster.eks_cluster]
 }
 
 output "cluster_security_group_id" {
-  value = module.eks.cluster_security_group_id
+  value = aws_security_group.eks_cluster_sg.id
 }
