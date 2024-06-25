05035ba9781e7a2ce311cbb681b3ed88ec69e921
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 22:15:43 +0800

    Update main.tf

--- aws_moodle/vpc/main.tf
+++ aws_moodle/vpc/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.4.0"
+  version = "3.10.0"  # Replace with the latest version if available
 
   name = "moodle-vpc"
   cidr = var.vpc_cidr
