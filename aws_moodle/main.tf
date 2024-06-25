68ddc749ee3cca2aecf64b98bc9c92757ebc317d
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 21:28:42 +0800

    Update main.tf

--- aws_moodle/main.tf
+++ aws_moodle/main.tf
@@ -3,42 +3,44 @@ module "vpc" {
 }
 
 module "eks" {
-  source = "./eks"
-  vpc_id = module.vpc.vpc_id
+  source          = "./eks"
+  vpc_id          = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnets
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
 }
 
 module "alb" {
-  source = "./alb"
-  vpc_id = module.vpc.vpc_id
+  source         = "./alb"
+  vpc_id         = module.vpc.vpc_id
   public_subnets = module.vpc.public_subnets
 }
 
 module "moodle" {
-  source = "./moodle"
-  moodle_admin_user = var.moodle_admin_user
-  moodle_admin_password = var.moodle_admin_password
-  moodle_admin_email = var.moodle_admin_email
-  mariadb_root_password = var.mariadb_root_password
-  mariadb_user = var.mariadb_user
-  mariadb_password = var.mariadb_password
-  mariadb_database = var.mariadb_database
-  private_subnets = module.vpc.private_subnets
-  alb_dns_name = module.alb.alb_dns_name
-  db_secrets_arn = module.secrets_manager.db_secrets_arn
+  source                  = "./moodle"
+  moodle_admin_user       = var.moodle_admin_user
+  moodle_admin_password   = var.moodle_admin_password
+  moodle_admin_email      = var.moodle_admin_email
+  mariadb_root_password   = var.mariadb_root_password
+  mariadb_user            = var.mariadb_user
+  mariadb_password        = var.mariadb_password
+  mariadb_database        = var.mariadb_database
+  private_subnets         = module.vpc.private_subnets
+  alb_dns_name            = module.alb.alb_dns_name
+  db_secrets_arn          = module.secrets_manager.db_secrets_arn
 }
 
 module "cloudfront_route53" {
-  source = "./cloudfront_route53"
+  source      = "./cloudfront_route53"
   alb_dns_name = module.alb.alb_dns_name
-  domain_name = var.domain_name
+  domain_name  = var.domain_name
 }
 
 module "elasticache_rds" {
-  source = "./elasticache_rds"
-  vpc_id = module.vpc.vpc_id
+  source          = "./elasticache_rds"
+  vpc_id          = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnets
-  db_secrets_arn = module.secrets_manager.db_secrets_arn
+  db_secrets_arn  = module.secrets_manager.db_secrets_arn
 }
 
 module "secrets_manager" {
