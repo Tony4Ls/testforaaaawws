d54478b5849ef53db5cf4d63ce4a207bac9b394c
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 20:40:05 +0800

    Update main.tf

--- aws_moodle/eks/main.tf
+++ aws_moodle/eks/main.tf
@@ -1,1 +1,33 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "18.0.0"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.21"
+  subnets         = var.private_subnets
+  vpc_id          = var.vpc_id
 
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_type    = "t3.medium"
+    }
+  }
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
+output "cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
