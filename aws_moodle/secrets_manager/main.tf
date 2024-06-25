560a103e9e005212bec41a64fd9477e12f243203
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 21:45:57 +0800

    Update main.tf

--- aws_moodle/secrets_manager/main.tf
+++ aws_moodle/secrets_manager/main.tf
@@ -1,6 +1,9 @@
 resource "aws_secretsmanager_secret" "moodle_db_secrets" {
   name = "moodle-db-secrets"
+}
 
+resource "aws_secretsmanager_secret_version" "moodle_db_secrets_version" {
+  secret_id     = aws_secretsmanager_secret.moodle_db_secrets.id
   secret_string = jsonencode({
     username = var.mariadb_user
     password = var.mariadb_password
