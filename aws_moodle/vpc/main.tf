edb1915e7f6004c2dbc79a20b7c31b16158cea20
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 20:41:08 +0800

    Create main.tf

---
+++ aws_moodle/vpc/main.tf
@@ -0,0 +1,36 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.4.0"
+
+  name = "moodle-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  tags = {
+    Name = "moodle-vpc"
+  }
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
