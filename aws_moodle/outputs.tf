bdc111ba22eaf0ffd67a7d9ab95f3fb8cb588296
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 20:38:49 +0800

    Create outputs.tf

---
+++ aws_moodle/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "eks_cluster_id" {
+  value = module.eks.cluster_id
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
+output "moodle_url" {
+  value = helm_release.moodle.status.load_balancer.ingress[0].hostname
+}
