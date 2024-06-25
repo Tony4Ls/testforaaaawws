907bc4dd765b7f35739573dd94333f2fe8bc5f63
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 23:20:05 +0800

    Update main.tf

--- aws_moodle/vpc/main.tf
+++ aws_moodle/vpc/main.tf
@@ -15,7 +15,6 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
   instance_tenancy     = "default"
-  assign_generated_ipv6_cidr_block = true
 
   tags = {
     Name = "moodle-vpc"
