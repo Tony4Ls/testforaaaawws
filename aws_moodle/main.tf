1ef16a735b3d0a934d9ff6be0a9900c61bd4c7e9
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 23:43:38 +0800

    Update main.tf

--- aws_moodle/main.tf
+++ aws_moodle/main.tf
@@ -12,32 +12,14 @@ module "eks" {
   private_subnets = module.vpc.private_subnets
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
+  cluster_security_group_id = module.vpc.cluster_security_group_id
 }
 
 module "alb" {
   source = "./alb"
   vpc_id = module.vpc.vpc_id
   public_subnets = module.vpc.public_subnets
-}
-
-module "moodle" {
-  source = "./moodle"
-  moodle_admin_user = var.moodle_admin_user
-  moodle_admin_password = var.moodle_admin_password
-  moodle_admin_email = var.moodle_admin_email
-  mariadb_root_password = var.mariadb_root_password
-  mariadb_user = var.mariadb_user
-  mariadb_password = var.mariadb_password
-  mariadb_database = var.mariadb_database
-  private_subnets = module.vpc.private_subnets
-  alb_dns_name = module.alb.alb_dns_name
-  db_secrets_arn = module.secrets_manager.db_secrets_arn
-}
-
-module "cloudfront_route53" {
-  source = "./cloudfront_route53"
-  alb_dns_name = module.alb.alb_dns_name
-  domain_name = var.domain_name
+  security_groups = [module.eks.cluster_security_group_id]
 }
 
 module "elasticache_rds" {
@@ -45,10 +27,25 @@ module "elasticache_rds" {
   vpc_id = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnets
   db_secrets_arn = module.secrets_manager.db_secrets_arn
+  security_groups = [module.eks.cluster_security_group_id]
 }
 
 module "secrets_manager" {
   source = "./secrets_manager"
   mariadb_user = var.mariadb_user
   mariadb_password = var.mariadb_password
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_iam_role" "lab_role" {
+  name = "LabRole"
+}
+
+variable "region" {
+  description = "The AWS region to deploy in"
+  type        = string
+  default     = "us-east-1"
 }
