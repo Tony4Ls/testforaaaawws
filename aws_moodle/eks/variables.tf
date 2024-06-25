93193fa67ff3260f213a8268ee84ac7f231f7e75
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 21:12:42 +0800

    Create variables.tf

---
+++ aws_moodle/eks/variables.tf
@@ -0,0 +1,20 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "private_subnets" {
+  description = "List of private subnet IDs"
+  type        = list(string)
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "EKS cluster version"
+  type        = string
+  default     = "1.30"
+}
