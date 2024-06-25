6539b732db5e8685ca82582254985f9fcb03bd38
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 21:57:53 +0800

    Create variables.tf

---
+++ aws_moodle/secrets_manager/variables.tf
@@ -0,0 +1,9 @@
+variable "mariadb_user" {
+  description = "Username for MariaDB"
+  type        = string
+}
+
+variable "mariadb_password" {
+  description = "Password for MariaDB"
+  type        = string
+}
