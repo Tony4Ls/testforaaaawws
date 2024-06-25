8dcd4066e09479c69951ef529a5d6b28370447ce
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-25 15:10:46 +0800

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,35 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "alb_dns_name" {
+  value = module.alb.alb_dns_name
+}
+
+output "cloudfront_domain_name" {
+  value = module.cloudfront_route53.cloudfront_domain_name
+}
+
+output "rds_endpoint" {
+  value = module.elasticache_rds.rds_endpoint
+}
+
+output "redis_endpoint" {
+  value = module.elasticache_rds.redis_endpoint
+}
+
+output "db_secrets_arn" {
+  value = module.secrets_manager.db_secrets_arn
+}
