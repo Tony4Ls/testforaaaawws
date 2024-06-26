dfc73100453b712188e9284014ad90888c8c2ab0
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-26 11:41:59 +0800

    Update main.tf

--- last/main.tf
+++ last/main.tf
@@ -137,8 +137,8 @@ resource "aws_eks_node_group" "eks_node_group" {
 
   scaling_config {
     desired_size = 2
-    max_size     = 3
-    min_size     = 0
+    max_size     = 4
+    min_size     = 1
   }
 
   instance_types = ["t3.medium"]
