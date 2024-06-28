abc43aece0319738a5ccc922786435d806cb79d6
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-28 23:00:06 +0800

    Update ebs.tf

--- terraform/ebs.tf
+++ terraform/ebs.tf
@@ -15,3 +15,21 @@ resource "aws_ebs_volume" "volume_b" {
     Name = "moodle-ebs-us-east-1b"
   }
 }
+
+resource "aws_ebs_volume" "volume_a" {
+  availability_zone = "us-east-1a"
+  size              = 10
+  type              = "gp2"
+  tags = {
+    Name = "mariadb-ebs-us-east-1a"
+  }
+}
+
+resource "aws_ebs_volume" "volume_b" {
+  availability_zone = "us-east-1b"
+  size              = 10
+  type              = "gp2"
+  tags = {
+    Name = "mariadb-ebs-us-east-1b"
+  }
+}
