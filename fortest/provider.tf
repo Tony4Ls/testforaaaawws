6ea5c5784f07c16c6daac8e4ef7f634e97f7c0e7
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-28 19:54:41 +0800

    Update provider.tf

--- fortest/provider.tf
+++ fortest/provider.tf
@@ -6,9 +6,6 @@ provider "kubernetes" {
   host                   = aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-
-  load_config_file = false
-  config_path      = "~/.kube/config"
 }
 
 data "aws_eks_cluster_auth" "cluster" {
