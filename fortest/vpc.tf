bdb5044580fc0a4e37490ce7a3407a26024d1235
Tony4Ls <144003217+Tony4Ls@users.noreply.github.com>
2024-06-28 19:55:05 +0800

    Update vpc.tf

--- fortest/vpc.tf
+++ fortest/vpc.tf
@@ -18,6 +18,17 @@ resource "aws_subnet" "private" {
   }
 }
 
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.moodle_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.moodle_vpc.cidr_block, 8, count.index + 2)
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "4122-public-subnet-${count.index}"
+  }
+}
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.moodle_vpc.id
   tags = {
